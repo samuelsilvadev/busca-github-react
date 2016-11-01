057e1aa7bcf8255154e4b021048281c855cadd3b
samuel <samuel@sotechsaudecom.br>
2016-11-01 13:29:20 -0300

    start project

---
+++ app/App.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+
+ReactDom.render(
+	<div className="jumbotron">
+  		<h1>Hello, world!</h1>
+  		<p>...</p>
+  		<p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+	</div>, 
+	document.getElementById('app'));
